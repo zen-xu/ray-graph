5df9d42580b3c375be32d3f98117ab636821419f
ZhengYu, Xu <zen-xu@outlook.com>
2025-01-21 17:32:53 +0800

    feat: support to get epoch from context (#48)
    * feat: support to get epoch from context
    
    * fix: lint

--- tests/unit/test_epoch.py
+++ tests/unit/test_epoch.py
@@ -1,6 +1,7 @@
 # ruff: noqa: ARG001
 from __future__ import annotations
 
+import pytest
 import sunray
 
 from ray_graph import (
@@ -34,3 +35,46 @@ def test_handle_epoch_event(init_ray):
     assert next(epoch_generator) == 0
     sunray.get(graph.get("node1").send(CustomEvent()))
     assert next(epoch_generator) == 1
+
+
+def test_get_epoch_from_context(init_ray):
+    class UpdateEpochEvent(Event): ...
+
+    class GetEpochEvent(Event[int]): ...
+
+    class CustomNode(RayAsyncNode):
+        @handle(UpdateEpochEvent)
+        async def handle_update_epoch(self, _event: UpdateEpochEvent) -> None:
+            ctx = get_node_context()
+            # notify epoch manager to next epoch
+            await ctx.graph.get(EPOCH_MANAGER_NAME).send(NextEpochEvent())
+
+        @handle(GetEpochEvent)
+        async def handle_get_epoch_event(self, _event: GetEpochEvent) -> int:
+            ctx = get_node_context()
+            return ctx.current_epoch
+
+    graph = RayGraphBuilder(
+        {"node1": CustomNode(), EPOCH_MANAGER_NAME: EpochManagerNode()}
+    ).build()
+    graph.start()
+    epoch_generator = epochs(graph)
+    assert next(epoch_generator) == 0
+    sunray.get(graph.get("node1").send(UpdateEpochEvent()))
+    assert next(epoch_generator) == 1
+    assert sunray.get(graph.get("node1").send(GetEpochEvent())) == 1
+
+
+def test_fail_to_get_epoch_from_context(init_ray):
+    class GetEpochEvent(Event[int]): ...
+
+    class CustomNode(RayAsyncNode):
+        @handle(GetEpochEvent)
+        async def handle_get_epoch_event(self, _event: GetEpochEvent) -> int:
+            ctx = get_node_context()
+            return ctx.current_epoch
+
+    graph = RayGraphBuilder({"node1": CustomNode()}).build()
+    graph.start()
+    with pytest.raises(ValueError, match=r"current epoch is not set"):
+        sunray.get(graph.get("node1").send(GetEpochEvent()))
